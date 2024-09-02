459ea77299208a6fdc1dc6a9bb1a399512dd1ea2
SamuVD <samuvv2905@gmail.com>
2024-09-02 07:25:17 -0500

    feat: cree la conexion entre la clase Owner y la clase Vehicle

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,19 @@ namespace ApiExample.Models;
 
 public class Vehicle
 {
+    [Key]
     public int Id { get; set; }
     public string Brand { get; set; }
     public string Model { get; set; }
+
+    // Referencia a la clase Propietario
+    public required int OwnerId { get; set; }
     public int Year { get; set; }
     public string Color { get; set; }
     public string TypeOfVehicle { get; set; }
+
+
+    // Referencia de la forein Key de la clase Propietario. Haciendo relación de uno a muchos. (Enlaces foraneos)
+    [ForeignKey("OwnerId")]
+    public Owner Owner { get; set; }
 }
