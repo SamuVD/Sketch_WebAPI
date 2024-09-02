929c8e8c37c6d29c51ae31360fba6a3f60de3e96
SamuVD <samuvv2905@gmail.com>
2024-09-02 07:09:33 -0500

    feat: estamos empezando a crear los modelos

---
+++ Models/Owner.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiExample.Models;
+
+public class Owner
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string IdNumber { get; set; }
+    public string Address { get; set; }
+    public string NumberPhone { get; set; }
+    public string Email { get; set; }
+}
