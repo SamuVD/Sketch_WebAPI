a3eaddf12a830e314bf5edcd1c3d04403e8f08d6
SamuVD <samuvv2905@gmail.com>
2024-09-02 07:13:42 -0500

    feat: llenando los models con las propiedades necesarias

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ApiExample.Models
+namespace ApiExample.Models;
+
+public class Vehicle
 {
-    public class Vehicle
-    {
-        
-    }
+    public int Id { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public string TypeOfVehicle { get; set; }
 }
