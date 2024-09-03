713d06db6e436675058af5115396ae7559ae28c1
SamuVD <samuvv2905@gmail.com>
2024-09-03 08:18:28 -0500

    feat: se agregaron migraciones

---
+++ Migrations/20240903131631_se_agregaron_validaciones_a_las_propiedades_de_la_clase_vehicle.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiExample.Migrations
+{
+    /// <inheritdoc />
+    public partial class se_agregaron_validaciones_a_las_propiedades_de_la_clase_vehicle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_Owners_OwnerId",
+                table: "Vehicles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vehicles",
+                table: "Vehicles");
+
+            migrationBuilder.RenameTable(
+                name: "Vehicles",
+                newName: "vehicles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Vehicles_OwnerId",
+                table: "vehicles",
+                newName: "IX_vehicles_OwnerId");
+
+            migrationBuilder.UpdateData(
+                table: "vehicles",
+                keyColumn: "TypeOfVehicle",
+                keyValue: null,
+                column: "TypeOfVehicle",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TypeOfVehicle",
+                table: "vehicles",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "vehicles",
+                keyColumn: "Model",
+                keyValue: null,
+                column: "Model",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "vehicles",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "vehicles",
+                keyColumn: "Color",
+                keyValue: null,
+                column: "Color",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "vehicles",
+                type: "varchar(80)",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "vehicles",
+                keyColumn: "Brand",
+                keyValue: null,
+                column: "Brand",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Brand",
+                table: "vehicles",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_vehicles",
+                table: "vehicles",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_vehicles_Owners_OwnerId",
+                table: "vehicles",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_vehicles_Owners_OwnerId",
+                table: "vehicles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_vehicles",
+                table: "vehicles");
+
+            migrationBuilder.RenameTable(
+                name: "vehicles",
+                newName: "Vehicles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_vehicles_OwnerId",
+                table: "Vehicles",
+                newName: "IX_Vehicles_OwnerId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TypeOfVehicle",
+                table: "Vehicles",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Vehicles",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "Vehicles",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(80)",
+                oldMaxLength: 80)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Brand",
+                table: "Vehicles",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vehicles",
+                table: "Vehicles",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_Owners_OwnerId",
+                table: "Vehicles",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
